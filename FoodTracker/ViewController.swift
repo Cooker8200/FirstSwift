96f0a574d4eae6d3dccb523b854e43750f3540a6
Matthew Cook <matthewcook@matthews-mbp>
2018-11-24 20:25:31 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Matthew Cook on 11/24/18.
+//  Copyright © 2018 Matthew Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
