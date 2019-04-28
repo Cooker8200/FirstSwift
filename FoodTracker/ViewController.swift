07cfcd40ceaee7a252e12ea753595a8cbd6c145b
Matthew Cook <matthewcook@matthews-mbp.attlocal.net>
2019-04-27 22:48:25 -0500

    working on screen updating on textfield end edit

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameTextField: UITextField!
+    @IBOutlet weak var defaultLabelTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        if (mealNameLabel.text == "Meal Name: ") {
+            mealNameLabel.text = "Default Text"
+        } else {
+            mealNameLabel.text = "Meal Name: "
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+        textField.text = ""
+    }
 }
 
